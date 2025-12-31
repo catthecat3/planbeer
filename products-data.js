752b58b72f6e984f2f33baee80ed63a7c9db7ae2
catthecat3 <153072826+catthecat3@users.noreply.github.com>
2025-12-31 10:40:25 +0300

    Create products-data.js

---
+++ products-data.js
@@ -0,0 +1,18 @@
+// products-data.js
+window.PRODUCTS_DATA = [
+  { 
+    name: 'IPA Classic', 
+    desc: '0.5 л • 6.5%', 
+    category: 'Крафтовое пиво', 
+    image: 'https://i.pinimg.com/originals/db/35/9a/db359a8ae87f8a076305374c30765d85.jpg', 
+    price: 350 
+  },
+  { 
+    name: 'Wheat Beer', 
+    desc: '0.5 л • 5.2%', 
+    category: 'Пиво', 
+    image: 'https://i.pinimg.com/originals/db/35/9a/db359a8ae87f8a076305374c30765d85.jpg', 
+    price: 320 
+  },
+  // Добавляйте товары здесь вручную
+];
